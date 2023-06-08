7c1ed503b9d0b238d92986a1693c6513db0f6a27
osdoonhyun <skw1996@naver.com>
2023-06-08 18:49:09 +0900

    헤더 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
+      <Header />
       <RouterProvider router={router} />
     </>
   );
