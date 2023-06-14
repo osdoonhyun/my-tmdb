4b643704102537255ac81c208ac35d5bbcc6109e
osdoonhyun <skw1996@naver.com>
2023-06-14 20:30:20 +0900

    로그인/회원가입 formContainer 추가

---
+++ src/components/AuthFormContainer.jsx
@@ -0,0 +1,12 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
+export default function AuthFormContainer({ title, children }) {
+  return (
+    <Container style={{ maxWidth: '400px' }}>
+      <h1 className='mt-5'>{title}</h1>
+      <Row className='mt-4'>
+        <Col>{children}</Col>
+      </Row>
+    </Container>
+  );
+}
